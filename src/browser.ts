c39f12b1ae7228c901690837ce824cfc186647d3
AEPKILL <a@aepkill.com>
2018-01-29 00:03:51 +0800

    修复: 判断 IE 浏览器错误

--- src/browser.ts
+++ src/browser.ts
@@ -1,7 +1,7 @@
 export const isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
 export const isIE =
   // edge
-  navigator.userAgent.indexOf('Edge') >= -1 ||
+  navigator.userAgent.indexOf('Edge') > -1 ||
   // ie11+
   navigator.userAgent.indexOf('Trident') > -1 ||
   // other ie browser
