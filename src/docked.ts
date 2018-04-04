fe9b012338ee7d81bcf8219e12f818a8f9c86a71
AEPKILL <a@aepkill.com>
2018-04-04 19:21:25 +0800

    optimize: 恰当的时间读取缩放信息

--- src/docked.ts
+++ src/docked.ts
@@ -3,11 +3,18 @@ import zoomDetail from './zoom-detail';
 const threshold = 160;
 
 export function devtoolsIsDocked() {
-  const zoom = zoomDetail().zoom;
-  return (
-    window.outerWidth - window.innerWidth * zoom > threshold ||
-    window.outerHeight - window.innerHeight * zoom > threshold
-  );
+  // 获取 zoom 是一个稍微耗时的操作，先预判一次再获取
+  if (
+    window.outerWidth - window.innerWidth > threshold ||
+    window.outerHeight - window.innerHeight > threshold
+  ) {
+    const zoom = zoomDetail().zoom;
+    return (
+      window.outerWidth - window.innerWidth * zoom > threshold ||
+      window.outerHeight - window.innerHeight * zoom > threshold
+    );
+  }
+  return false;
 }
 
 export default {
