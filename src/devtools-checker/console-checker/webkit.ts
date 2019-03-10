edb08f80f4a286d5a4ce75fb789848bbb67f96b1
AEPKILL <a@aepkill.com>
2019-03-10 14:34:42 +0800

    fix: reg-to-string-checker not work on webkit

--- src/devtools-checker/console-checker/webkit.ts
+++ src/devtools-checker/console-checker/webkit.ts
@@ -4,13 +4,11 @@ import { DevtoolsChecker } from '../devtools-checker';
 import depRegToStringChecker from './common/dep-reg-to-string';
 import elementIdChecker from './common/element-id';
 import functionToStringChecker from './common/function-to-string';
-import regToStringChecker from './common/reg-to-string';
 
 const webkitChecker: DevtoolsChecker = {
   ...checkerGroup([
     elementIdChecker,
     functionToStringChecker,
-    regToStringChecker,
     depRegToStringChecker
   ]),
   name: 'webkit-checker',
