bea74fdd67a8f7c4533a771e583b8ea5164e790c
AEPKILL <a@aepkill.com>
2021-05-09 13:19:27 +0800

    feat: export all checkers

---
+++ src/checkers/checkers.ts
@@ -0,0 +1,5 @@
+export { depRegToStringChecker } from './dep-reg-to-string.checker';
+export { elementIdChecker } from './element-id.checker';
+export { functionToStringChecker } from './function-to-string.checker';
+export { regToStringChecker } from './reg-to-string.checker';
+export { debuggerChecker } from './debugger.checker';
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import { DevtoolsDetector } from './classes/devtools-detector';
-import { debuggerChecker } from './checkers/debugger.checker';
-import { depRegToStringChecker } from './checkers/dep-reg-to-string.checker';
-import { elementIdChecker } from './checkers/element-id.checker';
-import { functionToStringChecker } from './checkers/function-to-string.checker';
-import { regToStringChecker } from './checkers/reg-to-string.checker';
 import { DevtoolsDetectorListener } from './types/devtools-detector-listener.type';
+import * as checkers from './checkers/checkers';
 
 const defaultDetector = new DevtoolsDetector({
   // 会按照 checker 的顺序执行检查
   checkers: [
-    elementIdChecker,
-    regToStringChecker,
-    functionToStringChecker,
-    depRegToStringChecker,
-    debuggerChecker,
+    checkers.elementIdChecker,
+    checkers.regToStringChecker,
+    checkers.functionToStringChecker,
+    checkers.depRegToStringChecker,
+    checkers.debuggerChecker,
   ],
 });
 
@@ -47,7 +43,7 @@ export { DevtoolsDetail } from './types/devtools-detail.type';
 export { DevtoolsDetectorListener } from './types/devtools-detector-listener.type';
 export { DevtoolsStatusChecker } from './types/devtools-status-checker.type';
 
-export { DevtoolsDetector };
+export { DevtoolsDetector, checkers };
 
 export * from './shared/browser-info';
 export * from './shared/console';
