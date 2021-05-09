448305daa95697db1ec384d9c870e4d73749e825
AEPKILL <a@aepkill.com>
2021-05-09 23:55:57 +0800

    feat: add `specificVersionMatch`

---
+++ src/constants/browser-specifics.const.ts
@@ -0,0 +1,7 @@
+export const MOZILLA = 'Mozilla';
+
+export const APPLE_WEBKIT = 'AppleWebKit';
+
+export const CHROME = 'Chrome';
+
+export const SAFARI = 'Safari';
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,6 @@
+import { compare, CompareOperator } from 'compare-versions';
+import { versionMap } from './version-map';
+
 export interface MatchOptions {
   includes?: boolean[];
   excludes?: boolean[];
@@ -24,4 +27,19 @@ export function match(options: MatchOptions = {}): boolean {
   }
 
   return included && !excluded;
+}
+
+export function specificVersionMatch(
+  specific: string,
+  firstVersion: string,
+  secondVersion: string,
+  operator: CompareOperator
+): boolean {
+  const version = versionMap[specific];
+
+  if (version === undefined) {
+    return false;
+  }
+
+  return compare(firstVersion, secondVersion, operator);
 }
